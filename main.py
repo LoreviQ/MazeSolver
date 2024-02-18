e3904a5d3a67ab8a928b91a5a319c5c6a343257d
LoreviQ <lorevi@protonmail.com>
2024-02-18 00:08:36 +0000

    Added in ability to draw lines

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+import time
 
 # Class for point
 class Point:
@@ -8,32 +9,33 @@ class Point:
 
 # Class for line
 class Line:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
 
-    def draw(self, canvas, fill_color):
-        canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2)
-        canvas.pack()
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
 
 # Class for GUI window
 class Window:
     def __init__(self, width, height):
-        #CONFIG
+        # CONFIG
         TITLE = "title"
 
-        #init
-        self.width = width
-        self.height = height
+        # init
         self.__root = Tk()
+        self.__root.geometry(f"{width}x{height}")
         self.__root.title(TITLE)
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
+        self.__canvas = Canvas()
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
     
+    # Draws line from input
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
     # Updates window
     def redraw(self):
         self.__root.update_idletasks()
@@ -41,17 +43,19 @@ class Window:
 
     # Constantly calls redraw() until window is closed
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
     
     # Called when window closed, stopping active code
     def close(self):
-        self.running = False
+        self.__running = False
         
 
 def main():
-    window = Window(400, 400)
+    window = Window(1000, 1000)
+    l1 = Line(Point(500,500),Point(1000,0))
+    window.draw_line(l1, "red")
     window.wait_for_close()
 
 if __name__ == "__main__":
